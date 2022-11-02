ac4078a8766af2d3969139942aa7f3b5b9fa3a22
Bruce Clark <bruce.clark55@gmail.com>
2022-11-01 20:15:07 -0500

    packages

--- main.js
+++ main.js
@@ -1,37 +1,74 @@
 const pinnedRepos = [
-{
-   id: 1,
-   name: "gatsby-workshop",
-   description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
-},
-{
-  id: 2,
-  name: "gatsby-workshop",
-  description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
-},
-{
-  id: 3,
-  name: "gatsby-workshop",
-  description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
-},
-{
-  id: 4,
-  name: "gatsby-workshop",
-  description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
-},
-{
-  id: 5,
-  name: "gatsby-workshop",
-  description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
-},
-{
-  id: 6,
-  name: "gatsby-workshop",
-  description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
-}
-
-]
+  {
+     id: 1,
+     name: "gatsby-workshop",
+     description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
+  },
+  {
+    id: 2,
+    name: "gatsby-workshop",
+    description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
+  },
+  {
+    id: 3,
+    name: "gatsby-workshop",
+    description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
+  },
+  {
+    id: 4,
+    name: "gatsby-workshop",
+    description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
+  },
+  {
+    id: 5,
+    name: "gatsby-workshop",
+    description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
+  },
+  {
+    id: 6,
+    name: "gatsby-workshop",
+    description: "This workshop covers the fundamentals of developing fast, accesible sites with Gatsby, a React framework, and dive deep into the building blocks you'll need to build your own custom Gatsby sites."
+  }
   
-const repoArray = [
+  ]
+    
+  const repoArray = [
+    
+  ]
   
-]
+  
+  const packages = [
+  {
+    id: 1,
+    name: "docker",
+    description: "a software platform used for building apps",
+  
+  },
+  {
+    id: 2,
+    name: "apache maven",
+    packageDescription: "a default package manager",
+  },
+  {  
+    id: 3,
+    name: "nuget",
+    description: "a software platform used for building apps",
+  },
+  
+  {
+    id: 4,
+    name: "ruby gems",
+    description: "a standard format for apps",
+  },
+  { 
+    id: 5,
+    name: "containers",
+    description: "a single place for your team",
+  },
+  { 
+    id: 6,
+    name: "npm",
+    description: "a single place for your team",
+  },
+  ];
+  
